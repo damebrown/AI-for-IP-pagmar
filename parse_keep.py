659b612a144375d68e046ebb9fcc4a66092a35ee
daniel.brown1 <daniel.brown1@mail.huji.ac.il>
2019-06-23 00:25:28 +0300

    workin'

--- parse_keep.py
+++ parse_keep.py
@@ -6,9 +6,14 @@ import gkeepapi
 import pandas as pd
 from pandas import plotting
 import csv
+import numpy as np
 import seaborn as sns
 
+NUMERIC_PATH = 'C:\\Users\\user\\PycharmProjects\\AIFIP_pagmar\\numeric_data.csv'
 PATH = 'C:\\Users\\user\\PycharmProjects\\AIFIP_pagmar\\data.csv'
+NOTEBOOK_PATH = 'C:\\Users\\user\\PycharmProjects\\AIFIP_pagmar\\'
+YESHIVA_1 ='yeshiva_1.csv'
+YESHIVA_2 ='yeshiva_2.csv'
 
 
 def update_keep(email, password):
@@ -100,78 +105,121 @@ def count_enters(poem):
 
 def parse_keep():
     keep = resume_last_keep()
+    # keep = update_keep('danielbrown13@gmail.com', 'hevelhavalimfor5777')
+    # save_keep(keep)
     notes = keep.all()
-    # shira_id = u'tag.etny4hklimae.1560181267354'
     counter = 0
-    hours, months, years = [], [], []
-    num_of_words, num_of_rows, num_of_houses, longest_row, shortest_row, = [], [], [], [], []
+    hours, days, months, years = [], [], [], []
+    num_of_words, num_of_rows, num_of_houses, longest_row, shortest_row, classes = [], [], [], [], [], []
     titles, broken_2_words, poem_texts = [], [], []
     for note in notes:
         if note.labels._labels != {}:
             if note.labels._labels[u'tag.etny4hklimae.1560181267354']:
+                counter += 1
                 time = note.timestamps.created
-                day = time.day
                 month = time.month
                 year = time.year
-                if year != 2019 and month != 6 and day != 16:
-                    # n_rows, n_words, n_houses, longest_row_length, shortest_row_length, broken_poem
-                    poem_texts.append(note.text)
-                    a, b, c, d, e, f = count_enters(note.text)
-                    num_of_rows.append(a), num_of_words.append(b), num_of_houses.append(c)
-                    longest_row.append(d), shortest_row.append(e), broken_2_words.append(f)
-                    hour = time.hour
-                    counter += 1
-                    hours.append(hour)
-                    months.append(month)
-                    years.append(year - 2000)
-                    title = int(bool(note.title))
-                    titles.append(title)
-                    print(time, hour, month, year, title)
-    data = {'Text': poem_texts,
-            'text broken to words': broken_2_words,
-            'titles': titles,
-            'hour': hours,
-            'month': months,
-            'year': years,
-            '# of words': num_of_words,
-            '# of rows': num_of_rows,
-            '# of houses': num_of_houses,
-            'longest row': longest_row,
-            'shortest row': shortest_row
-            }
+                # n_rows, n_words, n_houses, longest_row_length, shortest_row_length, broken_poem
+                poem_texts.append(note.text)
+                a, b, c, d, e, f = count_enters(note.text)
+                num_of_rows.append(a), num_of_words.append(b), num_of_houses.append(c)
+                longest_row.append(d), shortest_row.append(e), broken_2_words.append(f)
+                hours.append(time.hour)
+                days.append(time.day)
+                months.append(month)
+                years.append(year - 2000)
+                title = int(bool(note.title))
+                titles.append(title)
+                classes.append((year - 2013) * 12 + month)
+                # print(time, hour, month, year, title)
+    data = {
+        'titles': titles,
+        'hour': hours,
+        'day': days,
+        'month': months,
+        'year': years,
+        'class': classes,
+        '# of words': num_of_words,
+        '# of rows': num_of_rows,
+        '# of houses': num_of_houses,
+        'longest row': longest_row,
+        'shortest row': shortest_row,
+        'Text': poem_texts,
+        'text broken to words': broken_2_words
+    }
     df = pd.DataFrame(data, columns = cols)
     df.to_csv(PATH, encoding = 'utf-8-sig')
+    df = df.drop(['Text'], axis = 1)
+    df = df.drop(['text broken to words'], axis = 1)
+    df.to_csv(NUMERIC_PATH, encoding = 'utf-8-sig')
     return df
 
 
 def parse_notebooks(path):
-    months, years = [], []
-    num_of_words, num_of_rows, num_of_houses, longest_row, shortest_row, = [], [], [], [], []
+    hours, days, months, years = [], [], [], []
+    num_of_words, num_of_rows, num_of_houses, longest_row, shortest_row, classes = [], [], [], [], [], []
     titles, broken_2_words, poem_texts = [], [], []
-    poems = pd.read_csv(path,encoding='utf8')
-    poems = poems.to_numpy()
-    for poem in poems:
-        months.append(poem[1])
-        years.append(poem[2])
-        titles.append(poem[3])
+    pd_poems = pd.read_csv(path, encoding = 'utf8')
+    # pd_poems = pd.read_csv(path)
+    poems = pd_poems.to_numpy()
+    poems[:, 0] = pd_poems.iloc[:, 0]
+    for poem in pd_poems.iterrows():
+        print(poem[1][0])
+        poem_texts.append(poem[1][0])
+        month = poem[1][1]
+        year = poem[1][2]
+        hours.append(-1)
+        days.append(-1)
+        months.append(month)
+        years.append(year)
+        titles.append(poem[1][3])
+        classes.append((year - 13) * 12 + month)
         # n_rows, n_words, n_houses, longest_row_length, shortest_row_length, broken_poem
-        poem_texts.append(poem[0].encode())
-        a, b, c, d, e, f = count_enters(poem[0])
+        a, b, c, d, e, f = count_enters(poem[1][0])
         num_of_rows.append(a), num_of_words.append(b), num_of_houses.append(c)
         longest_row.append(d), shortest_row.append(e), broken_2_words.append(f)
+    data = {
+        'titles': titles,
+        'hour': hours,
+        'day': days,
+        'month': months,
+        'year': years,
+        'class': classes,
+        '# of words': num_of_words,
+        '# of rows': num_of_rows,
+        '# of houses': num_of_houses,
+        'longest row': longest_row,
+        'shortest row': shortest_row,
+        'Text': poem_texts,
+        'text broken to words': broken_2_words
+    }
+    df = pd.DataFrame(data, columns = cols)
+    df.to_csv(NOTEBOOK_PATH + YESHIVA_2, encoding = 'utf-8-sig')
+    return df
 
 
-cols = ['Text', 'text broken to words', 'titles', 'hour', 'month', 'year', '# of words', '# of rows', '# of houses',
-        'longest row', 'shortest row']
+string_cols = [
+    'Text',
+    'text broken to words']
+numeric_cols = ['titles', 'hour', 'day', 'month', 'year', 'class', '# of words', '# of rows', '# of houses',
+                'longest row', 'shortest row']
+cols = numeric_cols + string_cols
 filled_markers = ('o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X')
 
 # TODO- doc 2 vec
 # df = parse_keep()
-# df = pd.read_csv(PATH, names = cols)
+# df = pd.read_csv(NUMERIC_PATH, names = cols)
 # np_df = df.to_numpy()
-# np_df = np_df[1:, 2:]
-# numeric_df = pd.DataFrame(data = np_df, columns = cols[2:])
-# to_np = numeric_df.to_numpy()
+# np_df = np_df[1:, :-2].astype(int)
+# bins = max(np_df[:, 5])
+# plt.hist(np_df[:, 5], bins)
+# plt.grid(axis = 'x')
+# plt.xticks(np.arange(12, 78, step = 12))
+# plt.show()
 
-path_18_19 = "C:\\Users\\user\\PycharmProjects\\AIFIP_pagmar\\notebooks\\poems_18_19_2.csv"
-parse_notebooks(path_18_19)
+path_18_19 = "C:\\Users\\user\\PycharmProjects\\AIFIP_pagmar\\notebooks\\poems_18_19.csv"
+path_yeshiva_1 = "C:\\Users\\user\\PycharmProjects\\AIFIP_pagmar\\notebooks\\yeshive_poems_1.csv"
+path_yeshiva_2 = "C:\\Users\\user\\PycharmProjects\\AIFIP_pagmar\\notebooks\\yeshive_2_and_more.csv"
+df = parse_notebooks(path_yeshiva_2)
+npdf = df.to_numpy()
+print(1)
